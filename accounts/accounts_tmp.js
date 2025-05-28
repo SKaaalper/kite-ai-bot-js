66958153f98997b0e930c3a4c03bebfdf9af76f7
_skaaalper <159729426+SKaaalper@users.noreply.github.com>
2025-05-28 12:40:29 +0800

    Update accounts_tmp.js

--- accounts/accounts_tmp.js
+++ accounts/accounts_tmp.js
@@ -1,11 +1,3 @@
-/**
- * Private key list file
- * write your private key here like this
- * export const privateKey = [
- *   "PK1",
- *   "PK2",
- *   "PK3",
- * ];
- */
-
-export const privateKey = [];
+export const privateKey = [
+  "Your_Private_KEY" 
+];
