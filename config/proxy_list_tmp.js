b4f0d6582c4a4ef41996c2c81439a1c1ddf19ac4
_skaaalper <159729426+SKaaalper@users.noreply.github.com>
2025-05-28 12:41:21 +0800

    Update proxy_list_tmp.js

--- config/proxy_list_tmp.js
+++ config/proxy_list_tmp.js
@@ -1,11 +1,3 @@
-/**
- * THIS IS JUST EXAMPLE DON'T FILL YOUR PROXY LIST HERE
- * Example : 
- * export const proxyList = [
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-  ];
- */
-
-export const proxyList = [];
+export const proxylist = [
+  "Your_Proxy_List" 
+];
